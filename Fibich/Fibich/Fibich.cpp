a267b0c3b1b1e6e50b4770ff1d0797aa8cff618b
qod8bop <turcenkovtimofej99228@gmail.com>
2025-08-08 17:24:32 +0300

    Fibichi

---
+++ Fibich/Fibich/Fibich.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int Fibonaci(int n)	//По памяти 0
+{				    
+	if (n == 1) { return 0; }	
+	if (n == 2) { return 1; }
+	else { return (Fibonaci(n - 2) + Fibonaci(n - 1)); }
+}					//По времени O(n)  (2 проверки, 1 вывод, всё это вызывается 2n раз -> 6n)
+
+int main()
+{
+	std::cout << Fibonaci(4);
+
+	return 0;
+}
