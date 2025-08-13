a56485b30ab032baca37e544b1713477c682076d
qod8bop <turcenkovtimofej99228@gmail.com>
2025-08-13 18:24:44 +0300

    Pyr

---
+++ PrintPyramid/PrintPyramid/PrintPyramid.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+
+void print_pyramid(int* arr, int size)
+{
+
+	std::cout << "Исходный массив: ";
+	for(int i=0; i<size; i++)
+	{
+		std::cout << arr[i] << "  ";
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	int level = 0;
+
+	std::cout << level << "  " << "Root: " << arr[0] << std::endl << std::endl;
+
+	for(int i=0; i<size; i++)
+	{
+		level = log2(2*i + 1 + 1);
+
+		if (2 * i + 2 >= size) { std::cout << level << "  " << "Left (" << arr[i] << ") " << arr[2 * i + 1] << std::endl; break; }
+		if (2 * i + 1 >= size) { break; }
+
+		std::cout << level << "  " << "Left (" << arr[i] << ") " << arr[2 * i + 1] << std::endl;
+		std::cout << level << "  " << "Right(" << arr[i] << ") " << arr[2 * i + 2] << std::endl;
+		std::cout << std::endl;
+	}
+}
+
+
+int main()
+{
+
+	int arr[8] = { 94, 67, 18, 44, 55, 12, 6, 42 };
+
+	print_pyramid(arr, 8);
+
+	return 0;
+}
---
+++ PyramidNavigator/PyramidNavigator/PyramidNavigator.cpp
@@ -0,0 +1,12 @@
+﻿# include <iostream>
+
+
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	while(true)
+	{
+		std::cout << "Вы находитесь здесь: "
+	}
+}
